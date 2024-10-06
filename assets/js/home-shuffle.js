7dcba05fb03ac91023afbcc8df1f914fb7875d66
fantim <timfan@berkeley.edu>
2024-10-06 14:22:28 -0700

    home shuffle

--- assets/js/home-shuffle.js
+++ assets/js/home-shuffle.js
@@ -1,40 +1,69 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var Shuffle = window.Shuffle;
 
-    this.gridItems = this.element.querySelectorAll('.my-shuffle');
+var Shuffle = window.Shuffle;
+class Demo {
+    constructor() {
+        var myShuffle = new Shuffle(document.querySelector('.my-shuffle'), {
+            itemSelector: '.image-item',
+            // sizer: '.my-sizer-element',
+            columnWidth: (containerWidth) => containerWidth,
+            buffer: 1,
+        });
 
-    var myShuffle = new Shuffle(document.querySelector('.my-shuffle'), {
-        itemSelector: '.image-item',
-        // sizer: '.my-sizer-element',
-        columnWidth: (containerWidth) => containerWidth,
-        buffer: 1,
-    });
-
-    window.jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-        var input = evt.currentTarget;
-        if (input.checked) {
-            myShuffle.filter(input.value);
-        }
-    });
-
-    // Try Infinite Scroll
-    const callback = this.showItemsInViewport.bind(this);
-    this.observer = new IntersectionObserver(callback, {
-        threshold: 0.5,
-    });
-
-    // Loop through each grid item and add it to the viewport watcher.
-    for (let i = 0; i < this.gridItems.length; i++) {
-        this.observer.observe(this.gridItems[i]);
+        window.jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+            var input = evt.currentTarget;
+            if (input.checked) {
+                myShuffle.filter(input.value);
+            }
+        });
     }
+}
 
-    // Add the transition class to the items after ones that are in the viewport
-    // have received the `in` class.
-    setTimeout(() => {
-        this.addTransitionToItems();
-    }, 100);
-
+document.addEventListener('DOMContentLoaded', function () {
+    window.demo = new Demo(document.getElementById('.my-shuffle'));
 });
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//
+//     this.gridItems = this.element.querySelectorAll('.image-item');
+//
+//     var myShuffle = new Shuffle(document.querySelector('.my-shuffle'), {
+//         itemSelector: '.image-item',
+//         // sizer: '.my-sizer-element',
+//         columnWidth: (containerWidth) => containerWidth,
+//         buffer: 1,
+//     });
+//
+//
+//
+//     // Try Infinite Scroll
+//     const callback = this.showItemsInViewport.bind(this);
+//     this.observer = new IntersectionObserver(callback, {
+//         threshold: 0.5,
+//     });
+//
+//     // Loop through each grid item and add it to the viewport watcher.
+//     for (let i = 0; i < this.gridItems.length; i++) {
+//         this.observer.observe(this.gridItems[i]);
+//     }
+//
+//     // Add the transition class to the items after ones that are in the viewport
+//     // have received the `in` class.
+//     setTimeout(() => {
+//         this.addTransitionToItems();
+//     }, 100);
+//
+//         /**
+//      * Add the `in` class to the element after it comes into view.
+//      */
+//     showItemsInViewport(changes) {
+//         changes.forEach(function (change) {
+//             if (change.isIntersecting) {
+//                 change.target.classList.add('in');
+//             }
+//         });
+//     }
+//
+// });
 
 
 // class Demo {
