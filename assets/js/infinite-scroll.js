457198e6d54a09f2525e6bb89d061406223a1cdc
fantim <timfan@berkeley.edu>
2024-10-07 17:50:56 -0700

    change to use example

--- assets/js/infinite-scroll.js
+++ assets/js/infinite-scroll.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 // init Infinte Scroll
 
     let $container = $('.container').infiniteScroll({
-        path: '.pagination__next',
+        path: getPenPath,
         append: '.post',
         status: '.page-load-status',
     });
@@ -16,4 +16,18 @@ document.addEventListener('DOMContentLoaded', function () {
     $container.on('load.infiniteScroll', function () {
         $statusBar.text(`Loaded page: ${infScroll.pageIndex}`);
     });
+
+//-------------------------------------//
+// hack CodePen to load pens as pages
+
+    function getPenPath() {
+        const nextPenSlugs = [
+            '3d9a3b8092ebcf9bc4a72672b81df1ac',
+            '2cde50c59ea73c47aec5bd26343ce287',
+            'd83110c5f71ea23ba5800b6b1a4a95c4',
+        ];
+
+        let slug = nextPenSlugs[this.loadCount];
+        if (slug) return `/desandro/debug/${slug}`;
+    }
 });
