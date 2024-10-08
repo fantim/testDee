fd09623d7dd3c5aabcd30ff534f29818e74426ec
fantim <timfan@berkeley.edu>
2024-10-07 17:44:28 -0700

    add infinite scroll

--- assets/js/infinite-scroll.js
+++ assets/js/infinite-scroll.js
@@ -1,11 +1,8 @@
-<script src="https://unpkg.com/infinite-scroll@4/dist/infinite-scroll.pkgd.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
 document.addEventListener('DOMContentLoaded', function () {
 //-------------------------------------//
 // init Infinte Scroll
 
-    let $container = $('.container').infiniteScroll({
+    let $container = $('.container').InfiniteScroll({
         path: '.pagination__next',
         append: '.post',
         status: '.page-load-status',
