e6e5445b2399d9e385bc0d1f5593c18e8502c727
fantim <timfan@berkeley.edu>
2024-10-07 17:33:16 -0700

    add infinite scroll

--- assets/js/infinite-scroll.js
+++ assets/js/infinite-scroll.js
@@ -1,17 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
 //-------------------------------------//
 // init Infinte Scroll
 
-let $container = $('.container').infiniteScroll({
-    path: '.pagination__next',
-    append: '.post',
-    status: '.page-load-status',
-});
+    let $container = $('.container').infiniteScroll({
+        path: '.pagination__next',
+        append: '.post',
+        status: '.page-load-status',
+    });
 
 // get Infinite Scroll instance
-let infScroll = $container.data('infiniteScroll');
+    let infScroll = $container.data('infiniteScroll');
 
-let $statusBar = $('.status-bar');
+    let $statusBar = $('.status-bar');
 
-$container.on( 'load.infiniteScroll', function() {
-    $statusBar.text(`Loaded page: ${infScroll.pageIndex}`);
+    $container.on('load.infiniteScroll', function () {
+        $statusBar.text(`Loaded page: ${infScroll.pageIndex}`);
+    });
 });
